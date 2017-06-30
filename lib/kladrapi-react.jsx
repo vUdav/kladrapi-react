9591b81012c32d4ca163554566ede33709f58c5c
Валера Любимов <v.liubimov@pugofka.com>
2017-06-30 16:41:03 +0300

    Add jsonp

--- lib/kladrapi-react.jsx
+++ lib/kladrapi-react.jsx
@@ -1,9 +1,26 @@
+// Packages
 import React from 'react';
+import jsonp from 'jsonp';
+// Styles
 import './kladrapi-react.scss';
 
+class KladrApi extends Component {
+	// constructor(props) {
+	// 	super(props);
+	// 	this.state = {
+	//
+	// 	}
+	// }
 
-export default React.createClass({
-  render: function() {
-    return <div className="kladrapi-react">Hello World</div>;
-  }
-});
+	render() {
+		return (
+			<div className="kladr">
+				<input
+					type="text"
+				/>
+			</div>
+		)
+	}
+}
+
+export default KladrApi;
